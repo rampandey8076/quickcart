c4595e2fc3b5b7c9561434b6cd7d22e7092de771
rampandey8076 <rampandey92500@gmail.com>
2026-03-10 11:55:45 +0530

    make a cart

---
+++ react_application/src/components/ProductList.jsx
@@ -0,0 +1,15 @@
+import ProductCard from "./ProductCard";
+import products from "../data/products";
+import "../styles/ProductList.css";
+
+function ProductList() {
+  return (
+    <div className="grid">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
